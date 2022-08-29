836cf7f68dc1deb208666872599d9cfb991d4481
GitHub Action <action@github.com>
2022-08-29 09:59:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/linusmuema/slider/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/linusmuema/slider/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.linusmuema.slider.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.linusmuema.slider.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/linusmuema/slider/services/MyProjectService.kt
+++ src/main/kotlin/com/github/linusmuema/slider/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.linusmuema.slider.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.linusmuema.slider.MyBundle
 
 class MyProjectService(project: Project) {
 
